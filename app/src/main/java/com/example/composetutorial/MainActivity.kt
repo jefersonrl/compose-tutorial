514ac6a53ac73606ae36e3184a6fd71de05c85ce
jefersonrl <jeferson.lima17@etec.sp.gov.br>
2023-04-24 08:14:38 -0300

    second app compose

--- app/src/main/java/com/example/composetutorial/MainActivity.kt
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -16,7 +16,12 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           Text("Hello world!")
+            MessageCard("Android")
         }
     }
+}
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
 }
